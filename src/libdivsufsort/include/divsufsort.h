3eaf926c1a020cbb3bf90854a1eaf84c95234850
Emmanuel Marty <emmanuel@fgl.com>
2022-10-17 09:43:56 +0200

    Fix some documentation comments

--- src/libdivsufsort/include/divsufsort.h
+++ src/libdivsufsort/include/divsufsort.h
@@ -75,8 +75,8 @@ void divsufsort_destroy(divsufsort_ctx_t *ctx);
 /**
  * Constructs the suffix array of a given string.
  * @param ctx suffix array context
- * @param T[0..n-1] The input string.
- * @param SA[0..n-1] The output array of suffixes.
+ * @param T The input string.
+ * @param SA The output array of suffixes.
  * @param n The length of the given string.
  * @return 0 if no error occurred, -1 or -2 otherwise.
  */
